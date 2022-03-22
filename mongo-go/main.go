95874153d400687cc4cc708d03791f5bf0b2bb18
tburrell7 <tburrell7@gmail.com>
2022-03-21 22:17:52 -0400

    first commit but kinda late

---
+++ mongo-go/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	//"go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// You will be using this Trainer type later in the program
+
+
+func main() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	type Person struct {
+		Name string
+		Age  int
+	}
+	collection := client.Database("profile").Collection("people")
+
+	tom := Person{"Tom", 21}
+	insertResult, err := collection.InsertOne(context.TODO(), tom)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	// filter := bson.D{{"name", "Thomas"}}
+	// update := bson.D{
+	// 	{"$inc", bson.D{
+	// 		{"age", 1},
+	// 	}},
+	// }
+
+	// updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount)
+
+	// err = client.Disconnect(context.TODO())
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Connection to MongoDB closed.")
+}
